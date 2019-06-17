60340f9e220ec3f95123c01268829478621e9258
emmacallahan <callahan.emma.m@gmail.com>
2019-06-17 20:20:41 +0200

    Update how_many_vowels_in_string.py
    deleted s later in code to allow for any input

--- how_many_vowels_in_string.py
+++ how_many_vowels_in_string.py
@@ -1,7 +1,6 @@
 s = input ("Please enter a string:")
 number_of_vowels = 0
 number_of_consonants = 0
-s = 'azcbobobegghakl'
 for char in s:
     if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
         number_of_vowels += 1
