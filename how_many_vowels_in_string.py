4bb24c039dd6b10ce7b12ca1f5ab9681112f1616
emmacallahan <callahan.emma.m@gmail.com>
2019-06-19 12:37:02 +0200

    Update how_many_vowels_in_string.py
    clean up

--- how_many_vowels_in_string.py
+++ how_many_vowels_in_string.py
@@ -1,10 +1,7 @@
 s = input ("Please enter a string:")
 number_of_vowels = 0
-number_of_consonants = 0
+s = s.lower()
 for char in s:
-    if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' or char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U' :
+    if char in list(['a','e','i','o','u']):
         number_of_vowels += 1
-    else:
-        number_of_consonants += 1
-
 print ('Number of Vowels:' + str(number_of_vowels))
