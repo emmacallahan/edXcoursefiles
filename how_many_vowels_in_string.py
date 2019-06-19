6e0858618e80314e23c961b2e5f6a105965e517a
emmacallahan <callahan.emma.m@gmail.com>
2019-06-19 12:23:47 +0200

    Update how_many_vowels_in_string.py
    added uppercase

--- how_many_vowels_in_string.py
+++ how_many_vowels_in_string.py
@@ -2,7 +2,7 @@ s = input ("Please enter a string:")
 number_of_vowels = 0
 number_of_consonants = 0
 for char in s:
-    if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u' or char == 'A' or char == 'E' or char == 'I' or char == 'O' or char == 'U' :
         number_of_vowels += 1
     else:
         number_of_consonants += 1
